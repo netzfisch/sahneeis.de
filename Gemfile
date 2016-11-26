193603ac6f698c2c6fe1637fc171b5e169412f45
Harm Behrens <hb@netzfisch.de>
2016-11-26 17:23:05 +0100

    chore(base): sort config and Gemfile

--- Gemfile
+++ Gemfile
@@ -1,23 +1,23 @@
-source "https://rubygems.org"
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
 
 # Middleman Gems
 gem "middleman", "~> 4.1"
+gem 'middleman-livereload'
+gem "middleman-autoprefixer" #, "~> 2.6"
+gem "middleman-sprockets" #, "= 4.0.0.rc.1"
+gem "middleman-deploy", github: 'middleman-contrib/middleman-deploy', branch: 'master' #, "= 2.0.0.pre.alpha"
+
+# For blog extenstion
 gem "middleman-blog"
 gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+gem 'nokogiri'
+gem "builder", "~> 3.0" # For feed.xml.builder
 
-# Development
-gem "middleman-autoprefixer", "~> 2.6"
-gem "middleman-deploy", "= 2.0.0.pre.alpha"
-gem "middleman-livereload", "~> 3.4"
-gem "middleman-sprockets", "= 4.0.0.rc.1"
-
-# Design ...
+# Mixin SASS library, grid framework, scaffold styles and components
 gem "font-awesome-middleman"
 gem "bourbon", "~> 4.2"
 gem "neat", "~> 1.7"
 gem "bitters", "= 1.2"
 gem "sassc", "~> 1.8"
-
-# For feed.xml.builder
-gem "builder", "~> 3.0"
-
--- config.rb
+++ config.rb
@@ -1,5 +1,10 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
 activate :directory_indexes
 activate :autoprefixer
+activate :sprockets
 
 set :relative_links, true
 set :css_dir, "assets/stylesheets"
@@ -8,28 +13,31 @@ set :images_dir, "assets/images"
 set :fonts_dir, "assets/fonts"
 set :layout, "layouts/application"
 
+# Per-page layout changes:
+#
+# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-page "/feed.xml", layout: false
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
 
-configure :development do
- activate :livereload
-end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
-configure :build do
-  # Relative assets needed to deploy to Github Pages
-  activate :relative_assets
-end
+###
+# Helpers
+###
 
 activate :blog do |blog|
-  # set options on blog
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
-  blog.permalink = "{year}/{month}/{day}/{title}.html"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   blog.layout = "layouts/article"
   blog.summary_separator = /(READMORE)/
@@ -48,17 +56,14 @@ activate :blog do |blog|
   # blog.page_link = "page/{num}"
 end
 
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.deploy_method   = :sftp
-  deploy.host     = "sahneeis.de"
-  deploy.port     = 22
-  deploy.path     = "/sahneeis"
-  # Optional Settings
-  deploy.user     = ENV["USER"]
-  deploy.password = ENV["PASSWORD"]
+page "/feed.xml", layout: false
+
+# Reload the browser automatically whenever files change
+configure :development do
+ activate :livereload
 end
 
+# Methods defined in the helpers block are available in templates
 helpers do
   def nav_link(link_text, page_url, options = {})
     options[:class] ||= ""
@@ -70,4 +75,27 @@ helpers do
     options[:class] << " active" if page_url == current_url
     link_to(link_text, page_url, options)
   end
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.build_before    = true
+  deploy.deploy_method   = :sftp
+  deploy.host            = 'sahneeis.de'
+  deploy.port            = 22
+  deploy.path            = '/sahneeis'
+  # Optional Settings
+  deploy.user            = ENV["USER"]
+  deploy.password        = ENV["PASSWORD"]
 end
